f1e87f88ff7d2442c36c867575a7f930c073e831
CS949 <chaitanyas949@gmail.com>
2025-04-08 02:40:07 +0530

    first commit

---
+++ dags/etl_olist_dag.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import sys
+import os
+
+# Add the root project directory to sys.path so we can import etl_olist_project
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(PROJECT_PATH)
+
+from etl_olist_project import run_etl  # You must wrap your code inside run_etl() function
+
+default_args = {
+    'owner': 'chaitanya',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='olist_etl_kpis',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['olist', 'etl', 'kpi']
+) as dag:
+
+    run_etl_task = PythonOperator(
+        task_id='run_olist_etl',
+        python_callable=run_etl,
+    )
+
+    run_etl_task
